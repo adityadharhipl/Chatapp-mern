a274b67ab7718b0c147a81352e667c85179b490d
Aditya <adityadhar.hipl@gmail.com>
2026-06-25 15:24:45 +0530

    debug mongodb

--- server/src/config/db.js
+++ server/src/config/db.js
@@ -1,18 +1,31 @@
 import mongoose from "mongoose";
 
+// const connectDB = async () => {
+//   try {
+
+//     await mongoose.connect(
+//       process.env.MONGO_URI
+//     );
+
+//     console.log("MongoDB Connected ​🙂!!!");
+
+//   } catch (error) {
+
+//     console.log(error.message);
+
+//     process.exit(1);
+//   }
+// };
+
+// export default connectDB;
+
 const connectDB = async () => {
   try {
+    await mongoose.connect(process.env.MONGO_URI);
 
-    await mongoose.connect(
-      process.env.MONGO_URI
-    );
-
-    console.log("MongoDB Connected ​🙂!!!");
-
+    console.log("MongoDB Connected ✅");
   } catch (error) {
-
-    console.log(error.message);
-
+    console.error("MongoDB Error:", error);
     process.exit(1);
   }
 };
